22184948caac2a0519c4ff8df86ddc1877022bb8
Morteza Noshad <33552139+mrtnoshad@users.noreply.github.com>
2020-03-16 10:45:49 -0700

    Create unique_specialty_diagnosis.sql

---
+++ unique_specialty_diagnosis.sql
@@ -0,0 +1,19 @@
+WITH
+	specialtyNewPatientEncounters AS
+	(
+		select enc.jc_uid, enc.pat_enc_csn_id_coded as specialtyEncounterId, enc.appt_when_jittered as specialtyEncounterDateTime
+		from `starr_datalake2018.encounter` as enc join `starr_datalake2018.dep_map` as dep on enc.department_id = dep.department_id 
+		where dep.specialty_dep_c = '7' -- dep.specialty like 'Endocrin%'
+		and visit_type like 'NEW PATIENT%' -- Naturally screens to only 'Office Visit' enc_type 
+		-- and appt_type in ('Office Visit','Appointment') -- Otherwise Telephone, Refill, Orders Only, etc.
+		and appt_status = 'Completed'
+		and extract(YEAR from enc.appt_time_jittered) >= 2017	-- >= So capture follow-up visits in 2018 as well
+		-- 4796 records in 2017 (10650 for 2017 and after, largely including 2018)
+	)
+
+select DX.dx_name, count(*)
+		from specialtyNewPatientEncounters as specEnc
+		  join `starr_datalake2018.diagnosis_code` as DX on specEnc.specialtyEncounterId= DX.pat_enc_csn_id_coded
+		group by DX.dx_name
+		order by count(*) desc
+    
